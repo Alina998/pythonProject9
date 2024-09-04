01868f413445cfe84fcd7a47ba66b145aeb5a612
our ame <pastaeva98@mail.ru>
2024-09-04 22:11:19 +0700

    fixed code formatting

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,13 @@
-def filter_by_state(data, state='EXECUTED'):
-    """ Функция, которая фильтрует список словарей по ключу state """
-    return [item for item in data if item.get('state') == state]
+def filter_by_state(data: list, state="EXECUTED"):
+    """Функция, которая фильтрует список словарей по ключу state"""
+    return [item for item in data if item.get("state") == state]
 
 
 from datetime import datetime
 
 
-def sort_by_date(data, descending=True):
-    """ Функция, которая список по дате (по умолчанию — по убыванию) """
-    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
-
+def sort_by_date(data: list, descending=True):
+    """Функция, которая список по дате (по умолчанию — по убыванию)"""
+    return sorted(
+        data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=descending
+    )
